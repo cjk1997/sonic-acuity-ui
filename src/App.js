79112adb714e81ccc15d33e9eb722f8628b0dd13
Carson Keller <carsonjkeller@gmail.com>
2020-04-20 13:32:27 -0600

    Corrected imports and imported 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import logo from './logo.svg';
+// import './App.css';
+import GetTracks from './components/GetTracks';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>Sonic Acuity</h2>
+          <Route path="/" exact component={GetTracks}/>
+          {/* <Route path="/edit/:Id" component={EditTrial2}/> */}
+        </div>
+      </Router>
+    ); 
+  }
 }
 
 export default App;
