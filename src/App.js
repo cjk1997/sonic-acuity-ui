4fd7e04a6abc1559b42d18b6ce7e05d9bba51e4a
Carson Keller <carsonjkeller@gmail.com>
2020-04-22 08:42:58 -0600

    Corrected route path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-// import logo from './logo.svg';
-// import './App.css';
+// import logo from './init-components/logo.svg';
+// import './init-components/App.css';
 import GetTracks from './components/GetTracks';
 
 class App extends Component {
@@ -11,12 +10,12 @@ class App extends Component {
       <Router>
         <div className="container">
           <h2>Sonic Acuity</h2>
-          <Route path="/" exact component={GetTracks}/>
+          <Route path="/" exact component={ GetTracks }/>
           {/* <Route path="/edit/:Id" component={EditTrial2}/> */}
         </div>
       </Router>
     ); 
-  }
-}
+  };
+};
 
 export default App;
